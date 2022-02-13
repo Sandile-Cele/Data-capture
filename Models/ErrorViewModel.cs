7b74c9f35284a96ce23788194c6f357c442b2d5a
Sandile-Cele <Sandilecele101@gmail.com>
2022-02-13 17:57:53 +0200

    init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Data_capture.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
